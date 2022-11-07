17fd7b64741830c46be269f9253ad36054cef246
phillipe henriques <phillipe_ch@hotmail.com>
2022-11-07 20:25:36 -0300

    mfc

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function HomePage() {
+    const mensagem = "Deu certo essa desgraça?";
+    const estilosDaHomePage = { backgroundColor: "orange" };
+
+    return <div style={estilosDaHomePage}>{mensagem}</div>
+  }
+  
+  export default HomePage
+  
